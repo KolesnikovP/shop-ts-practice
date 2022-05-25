058ae136d48c5cbfb18254fe8f158dab0df2c348
Petr <ptrklsnkv@gmail.com>
2022-05-25 20:25:31 +0300

    промежуточный итог-допилить логику добавления в корзину и начать все переносить в редакс

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {IProduct} from '../../types';
 
-const ProductItem = () => {
+interface IProductItem {
+  product: IProduct
+}
+
+const ProductItem: FC<IProductItem> = ({product}) => {
+
+  const addHandler = () => {
+
+  }
+
   return (
-    <div>
-      
+    <div className='shadow-sm rounded-md'>
+      <img src={product.imagePath} alt={product.name}/>
+      <div className='text-lg'>{product.name}</div>
+      <div className='text-sm text-gray-600'>{product.price}</div>
+      <button onClick={()=> addHandler()}>В корзину</button>
     </div>
   );
 };
